099910605109a496bc3586afc1ad0694befbc9ac
Henrik Pettersson <h.pettersson@gmail.com>
2013-03-16 22:16:52 +0000

    Moved map init to the map view

--- static/js/app.js
+++ static/js/app.js
@@ -9,22 +9,9 @@ window.littlediary = {
         var self = this;
 
         app.model = new littlediary.Models.Application();
-
-        // Show loading indicator
-        $('#app').addClass('loading');
-
-        // Load map
-        mapbox.load('aj.Sketchy2', function(o) {
-            var map = mapbox.map('map');
-            map.center({ lat: -3, lon: 65 });
-            map.zoom(2.5);
-            map.addLayer(o.layer);
-
-            app.view = new littlediary.Views.Application({model: app.model, el: '#app'});
-            app.router = new littlediary.Routers.Application({model: app.model, view: app.view});
-            Backbone.history.start({pushState: true});
-            $('#app').removeClass('loading');
-        });
+        app.view = new littlediary.Views.Application({model: app.model, el: '#app'});
+        app.router = new littlediary.Routers.Application({model: app.model, view: app.view});
+        Backbone.history.start({pushState: true});
     }
 };
 

71544d22b594e26ce9f72c60c21d65064cf33528
Henrik Pettersson <h.pettersson@gmail.com>
2013-03-16 12:36:57 +0000

    Cleaned up handlers

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ import logging
 import os
 import webapp2
 
+
 def get_jinja2_template(path):
     template_dir = os.path.dirname(__file__)
     jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader([template_dir]))
     return jinja_environment.get_template(path)
+
 
 # http://remote.bergcloud.com/developers/reference/metajson
 class MetaJsonHandler(webapp2.RequestHandler):
@@ -49,6 +51,8 @@ class EditionHandler(webapp2.RequestHandler):
         delivery_time = self.request.get('local_delivery_time')
         delivery_count = int(self.request.get('delivery_count', 0))
 
+        # If delivery count is 0, show a welcome message
+
         # Find the entry of the day
 
         # Extract values
@@ -68,23 +72,30 @@ class EditionHandler(webapp2.RequestHandler):
 
 class SampleHandler(webapp2.RequestHandler):
     def get(self):
-        publication_issue = 1
-        emoji, title = get_publication_content(publication_issue)
+        # Extract values
         values = {
+            'chapter': '',
+            'entry': '',
+            'date': '',
+            'location': '',
+            'weather': '',
+            'wind': '',
         }
-        template = get_jinja2_template('publication.html')
+        template = get_jinja2_template('templates/edition.html')
+        
         self.response.out.write(template.render(values))
 
 
 class ValidateConfigHandler(webapp2.RequestHandler):
     def post(self):
-        pass # TODO
+        pass  # TODO
+
 
 # http://remote.bergcloud.com/developers/reference/configure
 class ConfigureHandler(webapp2.RequestHandler):
     def get(self):
-        return_url = self.request.get('return_url')
-    
+        pass  # TODO
+
 
 urls = [
     ('^/meta.json$', MetaJsonHandler),

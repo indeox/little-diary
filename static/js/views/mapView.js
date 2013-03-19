98fafbef96cb791402a10cab6a7b3f49e5a786e2
Henrik Pettersson <h.pettersson@gmail.com>
2013-03-19 23:19:43 +0000

    Calculate the angle between points using lat/long values

--- static/js/views/mapView.js
+++ static/js/views/mapView.js
@@ -28,47 +28,41 @@ littlediary.Views.Map = Backbone.View.extend({
         this.map.addLayer(this.markers);
 
         var markers = [];
-        _.each(data, function(point) {
-            var lat = parseFloat(point.location[0]),
-                lng = parseFloat(point.location[1]);
-                
+        _.each(data, function(point, index) {
+            var lat1 = parseFloat(point.location[0]),
+                lng1 = parseFloat(point.location[1]),
+                angle = 0;
+
+            var point2 = data[index + 1];
+            if (point2) {
+                var lat2 = parseFloat(point2.location[0]),
+                    lng2 = parseFloat(point2.location[1]),
+                    dLat = lat2 - lat1,
+                    dLng = lng2 - lng1;
+                angle = Math.atan2(dLng, dLat) * 180 / Math.PI;
+            }
+
             markers.push({
                 geometry: {
                     type: "Point",
                     coordinates: [
-                        lng,
-                        lat
-                    ]
+                        lng1,
+                        lat1
+                    ],
+                    angle: angle
                 }
             });
         }, this);
 
         this.markers.features(markers).factory(function(f) {
-            var span = document.createElement('span');
-            span.className = 'marker';
-            return span;
+            var div = document.createElement('div'),
+                span = document.createElement('span');
+            // span is needed because mapbox clobbers the rotate transform
+            div.appendChild(span);
+            div.className = 'marker';
+            $(span).css({rotate: f.geometry.angle + 'deg'});
+            return div;
         });
-
-        var nodes = this.$el.find('span.marker');
-        for (var i = 0, length = nodes.length; i < length; i++) {
-            var thisNode = nodes[i],
-                nextNode = nodes[i + 1];
-
-            if (nextNode) {
-                var pos1 = $(thisNode).position(),
-                    pos2 = $(nextNode).position();
-
-                var deltaY = pos2.top - pos1.top,
-                    deltaX = pos2.left - pos1.left;
-
-                var angle = Math.atan2(deltaY, deltaX) * 180 / Math.PI + 180;
-                
-                console.log(angle, deltaY, deltaX);
-
-                var transform = $(thisNode).css('-webkit-transform');
-                $(thisNode).css({'-webkit-transform': transform + 'rotate(' + angle + 'deg)'});
-            }
-        }
     }
 
 });

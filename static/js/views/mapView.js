2ccb104594f26bd14f6c69253bd41e8224c0f44c
Henrik Pettersson <h.pettersson@gmail.com>
2013-03-20 22:02:09 +0000

    Limit displayed route to go up to the current date. Added ship icon to last marker.

--- static/js/views/mapView.js
+++ static/js/views/mapView.js
@@ -10,7 +10,7 @@ littlediary.Views.Map = Backbone.View.extend({
 
         mapbox.load('aj.Sketchy2', function(o) {
             self.map = mapbox.map('map');
-            self.map.center({ lat: -3, lon: 65 });
+            self.map.center({ lat: 5, lon: -40 });
             self.map.zoom(2.5);
             self.map.addLayer(o.layer);
 
@@ -27,22 +27,31 @@ littlediary.Views.Map = Backbone.View.extend({
         this.markers = mapbox.markers.layer();
         this.map.addLayer(this.markers);
 
-        var markers = [];
-        _.each(data, function(point, index) {
-            var lat1 = parseFloat(point.location[0]),
-                lng1 = parseFloat(point.location[1]),
-                angle = 0;
+        var markers = [],
+            entry = this.model.get('currentEntry'),
+            currentDate = entry.get('date');
 
-            var point2 = data[index + 1];
+        // Only show the route up till today's date
+        for (var i = 0, length = data.length; i < length; i++) {
+            var point = data[i],
+                date = point.date,
+                lat1 = parseFloat(point.location[0]),
+                lng1 = parseFloat(point.location[1]),
+                angle = null;
+
+            var point2 = data[i + 1];
             if (point2) {
                 var lat2 = parseFloat(point2.location[0]),
                     lng2 = parseFloat(point2.location[1]),
                     dLat = lat2 - lat1,
                     dLng = lng2 - lng1;
-                angle = Math.atan2(dLng, dLat) * 180 / Math.PI;
+
+                if (dLat !== 0 || dLng !== 0) {
+                    angle = Math.atan2(dLng, dLat) * 180 / Math.PI;
+                }
             }
 
-            markers.push({
+            var feature = {
                 geometry: {
                     type: "Point",
                     coordinates: [
@@ -50,17 +59,31 @@ littlediary.Views.Map = Backbone.View.extend({
                         lat1
                     ],
                     angle: angle
-                }
-            });
-        }, this);
+                },
+                id: date
+            };
+            markers.push(feature);
+            if (date === currentDate) {
+                feature.isCurrent = true;
+                break;
+            }
+        }
 
         this.markers.features(markers).factory(function(f) {
             var div = document.createElement('div'),
                 span = document.createElement('span');
             // span is needed because mapbox clobbers the rotate transform
             div.appendChild(span);
+            div.id = f.id;
             div.className = 'marker';
-            $(span).css({rotate: f.geometry.angle + 'deg'});
+            if (f.isCurrent) {
+                div.className = 'marker ship';
+            }
+            if (f.geometry.angle) {
+                $(span).css({rotate: f.geometry.angle + 'deg'});
+            } else {
+                $(span).addClass('no-movement');
+            }
             return div;
         });
     }

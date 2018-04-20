9c774a8b412a1ca02f7af391d8063f5b15ed670e
David Vella <davevella@gmail.com>
2018-04-20 07:23:53 +0100

    Loop back to final year if we go before Aug 1768

--- static/js/models/appModel.js
+++ static/js/models/appModel.js
@@ -22,6 +22,14 @@ littlediary.Model.Application = Backbone.Model.extend({
 
     fetchEntry: function(date) {
         var entries = _.sortBy(this.get('entries').models, 'id');
+        var minDate = _.first(entries).get('date');
+        var maxDateYear = moment(_.last(entries).get('date')).year();
+
+        // Check if we're trying to get earlier than the first entry
+        // And if so, loop back to the final year (1771) - just a bit hacky O_O
+        if (moment(date).isBefore(minDate)) {
+            date = moment(date).year(maxDateYear).format('YYYY-MM-DD');
+        }
 
         // Remove all entries up till the required date
         entries = entries.filter(function(entry) {
